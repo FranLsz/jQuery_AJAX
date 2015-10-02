0755720041a62824267f5bf2fbee01559b1b51ed
FranLsz <franlsz95develop@gmail.com>
2015-10-02 11:47:24 +0200

    first update

---
+++ jQuery_Ajax/Scripts/app.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    var url = "https://alumnoscurso.azure-mobile.net/Tables/Alumno";
+
+    function conGet() {
+
+        $.get(url, function (res) {
+            var data = res;
+            var r =  "";
+            for (var i = 0; i < res.length; i++) {
+                r += data[i].nombre + "<br />";
+            }
+
+            $("#resultado").html(r);
+
+        });
+    }
+
+
+    $("#linkGet").click(function() {
+        conGet();
+
+    });
+
+
+})
