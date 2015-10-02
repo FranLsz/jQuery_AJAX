3b4399fb303d0cacaa374e627544646f7b157a66
FranLsz <franlsz95develop@gmail.com>
2015-10-02 12:15:42 +0200

    añadidas funciones y eventos

--- jQuery_Ajax/Scripts/app.js
+++ jQuery_Ajax/Scripts/app.js
@@ -1,24 +1,102 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
     var url = "https://alumnoscurso.azure-mobile.net/Tables/Alumno";
 
-    function conGet() {
-
-        $.get(url, function (res) {
-            var data = res;
-            var r =  "";
-            for (var i = 0; i < res.length; i++) {
-                r += data[i].nombre + "<br />";
-            }
-
-            $("#resultado").html(r);
-
-        });
+    function procesarJson(res) {
+        var data = res;
+        var r = "";
+        for (var i = 0; i < res.length; i++) {
+            r += data[i].nombre + "<br />";
+        }
+        $("#resultado").html(r);
     }
 
+    //recibe cualquier tipo de respuesta
+    function conGet() {
+        //peticion GET con url y la funcion que procesara la respuesta
+        $.get(url, procesarJson);
+    }
 
-    $("#linkGet").click(function() {
+    //SOLO procesa respuestas en formato JSON
+    function conGetJson() {
+        var o = {
+            nombre: "Fran",
+            apellidos: "Lopez",
+            edad: 20,
+            nota: 7
+        }
+        $.getJSON(url, procesarJson);
+    }
+
+    //no funciona con Azure, realiza una peticion POST
+    function conPost() {
+        var o = {
+            nombre: "Fran",
+            apellidos: "Lopez",
+            edad: 20,
+            nota: 7
+        }
+        $.post(url, JSON.stringify(o), function (res) {
+            console.log(res);
+        });
+    }
+    //inserta el contenido devuelto por LOAD (peticion get a esa URL) en un elemento HTML
+    function conLoad() {
+        $("#resultado").load(url);
+    }
+
+    //peticion AJAX configurable
+    function conAjax() {
+        var o = {
+            nombre: "Fran",
+            apellidos: "Lopez",
+            edad: 20,
+            nota: 7
+        }
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            success: function(res) {
+                console.log(res);
+            },
+            error: function(err) {
+                console.log(err);
+            },
+            data: JSON.stringify(o),
+            dataType: "",
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+
+    });
+
+    }
+    
+
+
+    $("#linkGet").click(function (e) {
+        e.preventDefault();
         conGet();
+    });
 
+    $("#linkGetJson").click(function (e) {
+        e.preventDefault();
+        conGetJson();
+    });
+    $("#linkPost").click(function (e) {
+        e.preventDefault();
+        conPost();
+    });
+
+    $("#linkLoad").click(function (e) {
+        e.preventDefault();
+        conLoad();
+    });
+
+    $("#linkAjax").click(function (e) {
+        e.preventDefault();
+        conAjax();
     });
 
 
